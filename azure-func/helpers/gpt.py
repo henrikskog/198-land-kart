7016ee9567c1d6c4790d59d8e6929a536a0d8b98
Henrik Skog <55615149+henrikskog@users.noreply.github.com>
2023-04-14 12:58:25 +0200

    removed unnused function

--- azure-func/helpers/gpt.py
+++ azure-func/helpers/gpt.py
@@ -5,29 +5,6 @@ from helpers.config import Config
 # Set up the OpenAI API credentials
 openai.api_key = Config().openai_api_key
 
-# Define the function
-def check_predicate(text, predicate):
-    # Send the text and predicate to the OpenAI GPT API to get a response
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Is the following true or false: {predicate} for {text}?",
-        max_tokens=10,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-
-    # Extract the answer from the response
-    answer = response.choices[0].text.strip().lower()
-
-    # Check if the predicate is true or false for the text
-    if answer == "true":
-        return True
-    elif answer == "false":
-        return False
-    else:
-        return None
-
 def extract_country(episode_name: str, episode_description: str):
     prompt = f"""
         You will be given an episode of an episode of a norwegian geography podcast. 
@@ -68,8 +45,6 @@ def extract_country(episode_name: str, episode_description: str):
     
     return country, cc
 
-
-# Define the function
 def get_countries_from_episodes(episodes):
     # Extract the names from each episode and put them in a list
     new = []
