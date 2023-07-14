e11e01ce1e1da40946ce6d2986f793f427b585bd
henrikskog <henrikskog01@gmail.com>
2023-07-14 23:32:32 +0200

    add list with countries

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const getCountryTranslations = async () => {
   return data;
 };
 
+const getCountryCoords = async () => {
+  // https://github.com/gavinr/world-countries-centroids/releases
+  const response = await fetch("country_general_data.json");
+  const data = await response.json();
+  return data;
+};
+
 const getGeoJsonData = async () => {
   const response = await fetch("country-geojson-data.json");
   const data = await response.json();
@@ -18,9 +25,9 @@ async function getEpisodes() {
   return data;
 }
 
-function createCountryPopup(countryName, cc, episodeData) {
+function createCountryPopupText(countryName, ISO_A3, episodeData) {
   let text = `<b>${countryName}</b>\n`;
-  const episodes = episodeData[cc];
+  const episodes = episodeData[ISO_A3];
 
   if (episodes) {
     episodes.forEach((entry) => {
@@ -41,34 +48,22 @@ const runOnFirstVisit = (callback) => {
   }
 };
 
-async function openRandomCountryOnLoad(
-  map,
-  geoJSONData,
-  countryTranslations,
-  episodeData
-) {
+async function openRandomCountryOnLoad(map, episodeData, countryCoords) {
   const countries = Object.keys(episodeData);
   const randomCountry = countries[Math.floor(Math.random() * countries.length)];
 
-  const episodes = episodeData[randomCountry];
-
-  const countryName =
-    countryTranslations[episodes[0].country] || episodes[0].country;
-  const popupText = createCountryPopup(countryName, randomCountry, episodeData);
-
-  const countryFeature = geoJSONData.features.find(
-    (feature) => feature.properties.ISO_A3 === randomCountry
+  const popupText = createCountryPopupText(
+    countryName,
+    randomCountry,
+    episodeData
   );
 
-  if (countryFeature) {
-    let countryCoordinates = L.geoJSON(countryFeature).getBounds().getCenter();
+  const countryCoordinates = L.latLng(
+    countryCoords[randomCountry].lat,
+    countryCoords[randomCountry].lon
+  );
 
-    if (randomCountry === "NOR") {
-      countryCoordinates = L.latLng(64.5, 13.5);
-    }
-
-    L.popup().setLatLng(countryCoordinates).setContent(popupText).openOn(map);
-  }
+  L.popup().setLatLng(countryCoordinates).setContent(popupText).openOn(map);
 }
 
 async function fetchWithProgress(url, totalSize, onProgress) {
@@ -108,7 +103,7 @@ async function fetchWithProgress(url, totalSize, onProgress) {
 
 const main = async () => {
   //   Fetching and parsing translations for country names from a JSON file.
-  const countryTranslations = await getCountryTranslations();
+  const countryData = await getCountryCoords();
 
   // Fetching GeoJSON data representing country shapes
   const geoJSONData = await fetchWithProgress(
@@ -124,11 +119,15 @@ const main = async () => {
   // Fetching and parsing an "episodes by country" dataset from a local JSON file.
   const episodeData = await getEpisodes();
 
+  const countryTranslations = await getCountryTranslations();
+
   // Setting up a Leaflet map, which is a JavaScript library for interactive maps.
   const map = L.map("map", {
     center: [62, 15],
     zoom: 3,
   });
+
+  fillCountryList(countryData, episodeData, map);
 
   // Attaching a tile layer to the map from OpenStreetMap, which provides the base map tiles.
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -142,7 +141,7 @@ const main = async () => {
       const name =
         countryTranslations[feature.properties.ADMIN] ||
         feature.properties.ADMIN;
-      const countryPopupText = createCountryPopup(
+      const countryPopupText = createCountryPopupText(
         name,
         feature.properties.ISO_A3,
         episodeData
@@ -167,9 +166,9 @@ const main = async () => {
   }).addTo(map);
 
   // On load, randomly selecting a country from the dataset and opening a popup with its information.
-  runOnFirstVisit(() =>
-    openRandomCountryOnLoad(map, geoJSONData, countryTranslations, episodeData)
-  );
+  runOnFirstVisit(async () => {
+    openRandomCountryOnLoad(map, episodeData, countryData);
+  });
 };
 
 // Error handling function
@@ -196,13 +195,13 @@ try {
   main().catch((error) => {
     console.log("Asynchronous error", error);
     handleError(
-      "UPS! Her skjedde det noe feil. :( Prøv å laste inn siden på nytt eller kom tilbake senere."
+      "🤒 Her har det skjedd noe feil! Prøv å laste inn siden på nytt eller kom tilbake senere."
     );
   });
 } catch (error) {
   console.log("Synchronous error", error);
   handleError(
-    "UPS! Her skjedde det noe feil. :( Prøv å laste inn siden på nytt eller kom tilbake senere."
+    "🤒 Her har det skjedd noe feil! Prøv å laste inn siden på nytt eller kom tilbake senere."
   );
 }
 
@@ -212,6 +211,7 @@ function updateProgress(number) {
     ".progress-bar-container"
   );
   const progressBar = document.getElementById("loader");
+  const map = document.getElementById("mapContainer");
 
   // Update the progress bar
   progressBar.value = number;
@@ -221,5 +221,71 @@ function updateProgress(number) {
     progressBarContainer.style.display = "block";
   } else {
     progressBarContainer.style.display = "none";
+    map.style.display = "block";
   }
 }
+
+// on click on     <button id="listBtn">📜</button>
+const listBtn = document.getElementById("listBtn");
+listBtn.addEventListener("click", () => {
+  const countryList = document.getElementById("countryList");
+  const disp = window.getComputedStyle(countryList).display;
+  console.log(disp);
+  if (disp == "none") {
+    countryList.style.display = "flex";
+    // add blur to map
+    const map = document.getElementById("map");
+    map.classList.add("blur");
+  } else {
+    countryList.style.display = "none";
+    map.classList.remove("blur");
+  }
+});
+
+const fillCountryList = (countryData, episodeData, map) => {
+  // Create a list of countries with norwegian names
+  const countryList = document.getElementById("countryList");
+  const ul = document.createElement("ul");
+  countryList.appendChild(ul);
+
+  // Loop through the countrytranslations
+  for (const [key, value] of Object.entries(countryData)) {
+    // Create a list item for each country
+    const li = document.createElement("li");
+    ul.appendChild(li);
+    li.innerHTML = value["name_no"];
+
+    // if in episodeData, add a class
+
+    const doneEpisodes = Object.keys(episodeData);
+
+    if (doneEpisodes.includes(value["ISO_A3"])) {
+      console.log("yes");
+      li.classList.add("episodeItem");
+      li.addEventListener("click", () => onListItemClick(value, map, episodeData));
+    } else {
+      li.classList.add("noEpisodeItem");
+    }
+  }
+};
+
+const onListItemClick = (countryName, map, episodeData) => {
+  const listModal = document.getElementById("countryList");
+  listModal.style.display = "none";
+
+  // remove blur from #map element
+  const mapEl = document.getElementById("map");
+  mapEl.classList.remove("blur");
+
+  const coords = [countryName.lat, countryName.lon];
+
+  map.setView(coords, 3);
+
+  const popupContent = createCountryPopupText(
+    countryName.name_no,
+    countryName.ISO_A3,
+    episodeData
+  );
+
+  L.popup().setLatLng(coords).setContent(popupContent).openOn(map);
+};
